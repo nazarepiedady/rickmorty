52d3b8274f19d82fc09bb081361905f8c7380484
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-12-16 13:36:14 +0100

    chore: define the properties of the character card component

--- components/CharacterCard.vue
+++ components/CharacterCard.vue
@@ -1,3 +1,27 @@
 <template></template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const properties = defineProps({
+  id: {},
+  name: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  species: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  }
+})
+</script>
