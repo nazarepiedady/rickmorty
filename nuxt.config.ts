afd0ae5464cc7fb01d0ab7d60ff30f5324fea6ad
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-12-16 13:03:09 +0100

    chore: comment all modules registration except nuxt apollo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/image-edge',
     '@nuxtjs/apollo',
-    '@nuxtjs/tailwindcss'
+    //'@nuxt/image-edge',
+    //'@nuxtjs/tailwindcss'
   ],
   apollo: {
     clients: {
-      httpEndpoint: 'https://rickandmortyapi.com/graphql'
+      default: {
+        httpEndpoint: 'https://rickandmortyapi.com/graphql'
+      }
     }
   },
 })
