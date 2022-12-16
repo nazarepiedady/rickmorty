9e56c14c15f6da251a016e21914f4571ef2bb487
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-12-16 13:04:18 +0100

    chore: create the query that returns the characters

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <p>{{ data }}</p>
   </div>
 </template>
+
+<script lang="ts" setup>
+const query = gql`
+  query getCharacters {
+    characters {
+      results {
+        id
+        name
+        image
+        status
+        species
+        location {
+          name
+        }
+      }
+    }
+  }
+`
+
+const { data } = await useAsyncQuery(query)
+</script>
